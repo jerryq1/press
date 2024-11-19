15f4d31a1cd902dd0ba57057b8c72a15bd4ebe04
jerry <xuzhanhong@gialen.com>
2024-11-19 11:05:08 +0800

    修改配置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,11 +2,14 @@
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
 import mediumZoom from 'medium-zoom';
-import { onMounted, watch, nextTick } from 'vue';
-import { useRoute } from 'vitepress';
+import { h,onMounted, watch, nextTick } from 'vue';
+import { useRoute,useData } from 'vitepress';
+import Archive from "./components/Archive.vue";
+
 export default {
     extends: DefaultTheme,
     setup() {
+        const { frontmatter } = useData();
         const route = useRoute();
         const initZoom = () => {
             // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
@@ -19,5 +22,24 @@ export default {
             () => route.path,
             () => nextTick(() => initZoom())
         );
+
+    },
+    Layout: () => {
+        const props: Record<string, any> = {};
+        // 获取 frontmatter
+        const { frontmatter } = useData();
+
+        /* 添加自定义 class */
+        if (frontmatter.value?.layoutClass) {
+            props.class = frontmatter.value.layoutClass;
+        }
+
+        return h(DefaultTheme.Layout, props);
+    },
+    enhanceApp({ app, router, siteData }) {
+        // 注册组件
+        // app.component("MNavLinks", MNavLinks);
+        // app.component("Navlink", Navlink);
+        app.component("Archive", Archive); // 全局注册归档组件
     },
 }
